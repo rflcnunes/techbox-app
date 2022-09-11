47fa8913254c3296ebfc8a17e5e32049ea6e7ddd
Rafael Nunes <rafaelcnunes@icloud.com>
2022-09-11 17:07:50 -0300

    Added Consumer

---
+++ techbox/app/RabbitMQ/Consumer/Consumer.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\RabbitMQ\Consumer;
+
+use Closure;
+use Exception;
+// use Bschmitt\Amqp\Amqp;
+// use Illuminate\Support\Facades\Log;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+// use PhpAmqpLib\Message\AMQPMessage;
+
+class Consumer implements ConsumerInterface
+{
+    // private $amqp;
+
+    // public function __construct(Amqp $amqp)
+    // {
+    //     $this->amqp = $amqp;
+    // }
+
+    public function consume($host, $queue, Closure $callback)
+    {
+        $connection = new AMQPStreamConnection($host, 5672, 'guest', 'guest');
+
+        $channel = $connection->channel();
+
+        $callback = function ($message) {
+            echo PHP_EOL . 'Consuming: ' . $message->body . PHP_EOL . PHP_EOL . PHP_EOL . 'Press CTRL+C to exit' . PHP_EOL;
+        };
+
+        $channel->basic_consume($queue, '', false, true, false, false, $callback);
+        // $channel->basic_consume('post_log_queue', '', false, true, false, false, $callback);
+        // $channel->basic_consume('sendEmail', '', false, true, false, false, $callback);
+
+        while ($channel->is_consuming()) {
+            $channel->wait();
+        }
+
+        $channel->close();
+        $connection->close();
+    }
+}
