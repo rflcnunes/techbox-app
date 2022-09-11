aa8e852b913718397864f42db997b98fa9d4a66d
Rafael Nunes <rafaelcnunes@icloud.com>
2022-09-11 03:13:28 -0300

    Relocation of repository and added namespace in routes

--- techbox/routes/api.php
+++ techbox/routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\CommentController;
 
 /*
 |--------------------------------------------------------------------------
