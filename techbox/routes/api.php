1b3710329c6e8952e9830bc58c1b2c680b437bb5
Rafael Nunes <rafaelcnunes@icloud.com>
2022-09-11 03:07:52 -0300

    Added routes

--- techbox/routes/api.php
+++ techbox/routes/api.php
@@ -17,3 +17,18 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix' => 'users'], function () {
+    Route::get('/', [UserController::class, 'index']);
+    Route::get('/{id}', [UserController::class, 'show']);
+});
+
+Route::group(['prefix' => 'posts'], function () {
+    Route::get('/', [PostController::class, 'index']);
+    Route::get('/{id}', [PostController::class, 'show']);
+});
+
+Route::group(['prefix' => 'comments'], function () {
+    Route::get('/', [CommentController::class, 'index']);
+    Route::get('/{id}', [CommentController::class, 'show']);
+});
