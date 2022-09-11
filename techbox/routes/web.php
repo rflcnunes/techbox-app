24ac26f72d6cee9e36cd2368c42922bfbf37995a
Rafael Nunes <rafaelcnunes@icloud.com>
2022-09-11 01:27:41 -0300

    Added publisher and consumer for rabbitmq

--- techbox/routes/web.php
+++ techbox/routes/web.php
@@ -24,10 +24,46 @@ Route::get('/send', function () {
     $connection = new AMQPStreamConnection('rabbitmq', 5672, 'guest', 'guest');
 
     $channel = $connection->channel();
-    $channel->queue_declare('sendEmail', false, true, false, false);
+    // $channel->queue_declare('sendEmail', false, true, false, false);
 
-    $rabbitMsg = new AMQPMessage('Hello World!');
-    $channel->basic_publish($rabbitMsg, '', 'sendEmail');
+    // $rabbitMsg = new AMQPMessage('Hello World!');
+    // $channel->basic_publish($rabbitMsg, '', 'sendEmail');
+
+    $channel->exchange_declare('posts_events', 'direct');
+
+    $channel->queue_declare('create_post_queue');
+    $channel->queue_declare('post_log_queue');
+
+    $channel->queue_bind('create_post_queue', 'posts_events', 'create_post');
+    $channel->queue_bind('post_log_queue', 'posts_events', 'post_log');
+
+    $firstMessage = new AMQPMessage('Create Post');
+    $secondMessage = new AMQPMessage('Post Log');
+
+    $channel->basic_publish($firstMessage, 'posts_events', 'create_post');
+    $channel->basic_publish($secondMessage, 'posts_events', 'post_log');
+
+    $channel->close();
+    $connection->close();
+});
+
+Route::get('/consumer', function () {
+
+    $connection = new AMQPStreamConnection('rabbitmq', 5672, 'guest', 'guest');
+
+    $channel = $connection->channel();
+
+    $callback = function ($message) {
+        echo $message->body;
+    };
+    
+    $channel->basic_consume('create_post_queue', '', false, true, false, false, $callback);
+    $channel->basic_consume('post_log_queue', '', false, true, false, false, $callback);
+    // $channel->basic_consume('sendEmail', '', false, true, false, false, $callback);
+
+    while ($channel->is_consuming()) {
+        $channel->wait();
+    }
 
     $channel->close();
     $connection->close();
